2b40e8775570130133836a83cfb1cfdf3854cb4f
Dickson Law <dickson.law@utoronto.ca>
2018-02-20 12:19:03 -0500

    Fix typo in mirador directory check

--- setup/mirador.sh
+++ setup/mirador.sh
@@ -5,7 +5,7 @@ echo "IIIF API in a Box: Installing Mirador..."
 
 # Download Mirador if not already provisioned
 cd /var/www
-if [ ! -d iiifAPI ]; then
+if [ ! -d mirador ]; then
 	echo "Provisioning from GitHub Mirador 2.6.0..."
 	wget -q --no-check-certificate https://github.com/ProjectMirador/mirador/releases/download/v2.6.0/build.zip
 	unzip -qq build.zip 'mirador/*' -d mirador
