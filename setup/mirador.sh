9d0bfa9db7071e9d0445758a04506da86f7b0f91
Dickson Law <dickson.law@utoronto.ca>
2018-03-26 17:30:32 -0400

    Point Mirador to our lazy collection build
    Download Mirador from the built version at GitHub
    (utlib/develop_collection_tree_lazy).

--- setup/mirador.sh
+++ setup/mirador.sh
@@ -7,8 +7,8 @@ echo "IIIF API in a Box: Installing Mirador..."
 cd /var/www
 if [ ! -d mirador ]; then
 	echo "Provisioning from GitHub Mirador 2.6.0..."
-	wget -q --no-check-certificate https://github.com/ProjectMirador/mirador/releases/download/v2.6.0/build.zip
-	unzip -qq build.zip 'mirador/*' -d mirador > /dev/null 2>&1
+	wget -q --no-check-certificate https://github.com/utlib/mirador/releases/download/2.6-lazy-collection-tree/build.zip
+	unzip -qq build.zip 'mirador/*' > /dev/null 2>&1
 	rm -Rf build.zip
 fi
 
